37cf79c5f3b7eb229a342ea04826bf9a30a3615a
Steve Loo <isignin@gmail.com>
2015-12-15 22:57:59 -0500

    Creating an app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,39 @@
-var EddCalculator = new Marionette.Application();
+var EddApp = new Marionette.Application();
+  
+  EddApp.addRegions({
+	  headerRegion: "#header-region",
+	  iconRegion: "#menu-icons",
+	  inputRegion: "#input-box",
+	  resultRegion: "#calc-result"
+  });
 
-EddCalculator.addRegions({
-  headerRegion: "#header-region",
-  mainRegion: "#main-region",
-  inputsRegion: "#input_box",
-  resultRegion: "#calc_result",		
-  dialogRegion: Marionette.Region.Dialog.extend({
-    el: "#dialog-region"
-  })
-});
+  EddApp.HeaderView = Marionette.ItemView.extend({
+	      template: "#header-template"
+	  });
+	  
+   EddApp.IconsView = Marionette.ItemView.extend({
+	      template: "#menuIcons-template"
+	  });
+	  
+	  EddApp.InputsView = Marionette.ItemView.extend({
+	      template: "#inputs-template"
+	  });
+	  
+	  EddApp.ResultsView = Marionette.ItemView.extend({
+	      template: "#results-template"
+	  });
+	
+	EddApp.on("initialize:after", function(){ 
+		  var headerView = new EddApp.HeaderView(); 
+		  var iconsView = new EddApp.IconsView();
+		  var inputsView = new EddApp.InputsView();
+		  var resultsView = new EddApp.ResultsView();
 
-EddCalculator.navigate = function(route,  options){
-  options || (options = {});
-  Backbone.history.navigate(route, options);
-};
-
-EddCalculator.getCurrentRoute = function(){
-  return Backbone.history.fragment
-};
-
-EddCalculator.on("initialize:after", function(){
-  if(Backbone.history){
-    Backbone.history.start();
-/*
-    if(this.getCurrentRoute() === ""){
-      ContactManager.trigger("contacts:list");
-    }
-*/	
-  }
-});
+		  EddApp.headerRegion.show(headerView);
+		  EddApp.iconRegion.show(iconsView);
+		  EddApp.inputRegion.show(inputsView);
+		  EddApp.resultRegion.show(resultsView);
+		var defLang = new EddApp.Entities.Language();
+		console.log(defLang.en);
+	    });
+	
